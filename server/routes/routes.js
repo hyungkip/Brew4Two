86ad39f9f9f2e02087c80cd3185f41c8ed82a2f9
Eleanor Demis <eleanordemis@gmail.com>
2016-03-01 19:41:28 -0800

    [bug] adds modular appointment controller

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -9,13 +9,13 @@ module.exports = function(app, express) {
   app.use('/', express.static(__dirname + '../../app'));
 
 //appointments controller
-  app.post('/createAppointment', jsonParser, '....');
-  app.post('/getAppointments', jsonParser, '....');
-  app.post('/filterAppointments', jsonParser, '...');
-  app.post('/sendJoinRequest', jsonParser, '...');
-  app.get('/fetchAppointmentsDashboardData', jsonParser, '...');
-  app.post('/acceptAppt', jsonParser, '....');
-  app.post('/denyAppt', jsonParser, '....');
+  app.post('/createAppointment', jsonParser, apptController.createAppt);
+  app.post('/getAppointments', jsonParser, apptController.getAppts);
+  app.post('/filterAppointments', jsonParser, apptController.filterAppts);
+  app.post('/sendJoinRequest', jsonParser, apptController.joinAppt);
+  app.get('/fetchAppointmentsDashboardData', jsonParser, apptController.fetchDashboardData);
+  app.post('/acceptAppt', jsonParser, apptController.acceptAppt);
+  app.post('/denyAppt', jsonParser, apptController.denyAppt);
 
 //user controller
   app.post('/signup', jsonParser, userController.signup);
