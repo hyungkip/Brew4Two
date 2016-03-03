9d9cdaf16830cb1e14c6fdea0dc962f16641ecab
bllnd <bllnd@mac.com>
2016-03-02 19:36:14 -0800

    [feat] implements username validation server side

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -13,6 +13,9 @@ module.exports = {
     req.body.password = hash;
 
     db.users.find({username: username}, function(err, exists) {
+      if (exists.length) {
+
+      };
       if(!exists.length){
         db.users.insert(req.body, function(err, doc) {
           if(err){
