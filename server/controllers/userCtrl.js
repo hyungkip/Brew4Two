d3c0fc6d5d0a7f5b9ae7a02997260c1114231007
bllnd <bllnd@mac.com>
2016-03-03 12:06:47 -0800

    [feature] implement user alert on duplicate username

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -14,18 +14,20 @@ module.exports = {
     db.users.find({username: username}, function(err, exists) {
       if(!exists.length){
         db.users.insert(req.body, function(err, doc) {
-          if(err){
-            console.log('++line 19 inside userCtrl.js !exists.length',err);
-          }
+          console.log('++line 17 inside userCtrl.js upon user insertion', req.body);
+          // if(err){
+          //   console.log('++line 19 inside userCtrl.js !exists.length',err);
+          // }
         });
+          res.send('/signin');
       }
       else {
-        console.log('++line 24 inside userCtrl.js else');
+        console.log('++line 25 inside userCtrl.js else');
+        alert('User exists!');
 
       }
     });
 
-    res.send('/signin');
   },
 
   renderSignin: function(req, res) {
